f0ba57042bcf050c54a24714a2b45d9628523181
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-27 18:10:16 -0300

    config root path for jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleDirectories: ["node_modules", "<rootDir>"],
 };
 
 export default config;
