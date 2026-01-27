ee1b096dffbcebd38325b56522dd749dc5956617
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-27 14:42:45 -0300

    `/status` endpoint done

--- pages/api/v1/status/index.tsx
+++ pages/api/v1/status/index.tsx
@@ -1,9 +1,35 @@
-import { Request, Response } from "express";
+import database from "infra/database";
 
-async function status(req: Request, res: Response): Promise<Response> {
+async function status(req, res) {
   const updatedAt = new Date();
 
-  return res.status(200).json({ updated_at: updatedAt });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnectionsValue = databaseMaxConnectionsResult[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
