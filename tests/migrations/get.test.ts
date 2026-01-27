bcdf0c46fa4b2ae67dd0baebd3207bbd503ac299
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-27 18:08:35 -0300

    add `get.test.ts` `post.test.ts` endpoint tests

--- tests/migrations/get.test.ts
+++ tests/migrations/get.test.ts
@@ -1,6 +1,18 @@
-test("test", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+import database from "infra/database";
 
-  console.log(response);
+beforeAll(async () => {
+  await database.query("drop schema public cascade; create schema public;");
+});
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "GET",
+  });
+
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
