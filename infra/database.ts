90b39b63b6e81bb839ff682b7848df3bead0bf2a
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-27 18:56:15 -0300

    test2

--- infra/database.ts
+++ infra/database.ts
@@ -17,6 +17,14 @@ async function query(queryObject) {
   }
 }
 
+console.log("Credentials:", {
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -26,13 +34,6 @@ async function getNewClient() {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  console.log("Credentials:", {
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
   await client.connect();
   return client;
 }
