9c1eb7c9895ff4545129f5a32b38947d44023958
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-27 18:11:22 -0300

    `/migrations` endpoint done

--- pages/api/v1/migrations/index.tsx
+++ pages/api/v1/migrations/index.tsx
@@ -1,9 +1,47 @@
+import { runner } from "node-pg-migrate";
 import database from "infra/database";
+import { resolve } from "node:path";
 
-async function migrations(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
+export default async function runMigrations(req, res) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(req.method)) {
+    return res
+      .status(405)
+      .json({ error: `Method "${req.method}" not allowed` });
+  }
 
-  res.send(result);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationsOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: resolve("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    } as const;
+
+    if (req.method === "GET") {
+      const pendingMigrations = await runner(defaultMigrationsOptions);
+      return res.status(200).json(pendingMigrations);
+    }
+    if (req.method === "POST") {
+      const migratedMigrations = await runner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return res.status(201).json(migratedMigrations);
+      }
+      return res.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
-
-export default migrations;
