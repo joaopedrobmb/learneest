743aa07008993c8ab2a23d8273fb3838a3b241f2
joaopedrobmb <joaopedrobmb@gmail.com>
2026-02-09 19:37:50 -0300

    feat: add logo to favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import "../src/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
