2969ce9c375738988ab62067280e2d2cb8e86154
joaopedrobmb <joaopedrobmb@gmail.com>
2026-03-12 18:08:01 -0300

    feat: add `create study_sessions` migrations

---
+++ infra/migrations/1773344366893_create-study-sessions.js
@@ -0,0 +1,39 @@
+exports.up = (pgm) => {
+  pgm.createTable("study_sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+
+    subject: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+
+    scheduled_start: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    scheduled_end: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
+    },
+  });
+};
+
+exports.down = false;
